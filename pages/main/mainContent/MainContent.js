dbe700f6163aba4165b0eb045cfcf1220937ab8b
elaltas <elaltas@gmail.com>
2021-10-14 02:46:13 +0200

    functional interface

---
+++ pages/main/mainContent/MainContent.js
@@ -0,0 +1,38 @@
+import phone from './../../static/images/phone.png';
+import logoGoogle from './../../static/images/gPlay.png';
+import logoApple from './../../static/images/appStore.png';
+import loginFake from './../../static/images/loginFake.png';
+import Image from 'next/image';
+
+export default function MainContent() {
+  return (
+    <div>
+      <div class="row">
+        <div class="column">
+          <div className="centered">
+            <Image src={phone} className="styleIphone" />
+          </div>
+        </div>
+        <div class="column">
+          <div className="centered">
+            <Image src={loginFake} />
+            <h4 className="instaFont"> Descarga la aplicación.</h4>
+
+            <div class="column">
+              <div className="centered">
+                <Image src={logoApple} className="styleBtns" />
+              </div>
+            </div>
+            <div class="column">
+              <div className="centered">
+                <Image src={logoGoogle} className="styleBtns" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
