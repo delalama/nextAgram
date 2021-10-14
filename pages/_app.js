695c06c2ef3eccc5abb9e881de29ce8f2b2f7847
elaltas <elaltas@gmail.com>
2021-10-14 02:45:21 +0200

    init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import './main/footer/footer.css';
+import './main/mainContent/MainContent.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/main/footer/link.js
@@ -0,0 +1,55 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+const linksName = [
+  'Información',
+  'Blog',
+  'Empleo',
+  'Ayuda',
+  'API',
+  'Privacidad',
+  'Condiciones',
+  'Cuentas destacadas',
+  'Hashtags',
+  'Ubicaciones',
+  'Instagram Lite',
+  'Belleza',
+  'Danza',
+  'Actividad física',
+  'Comida y bebida',
+  'Hogar y jardinería',
+  'Música',
+  'Artes visuales',
+];
+
+export default function InstaFooter() {
+  return (
+    <Container className="separateBottom">
+      <div width='50vw'>
+        {linksName.map((name) => {
+          return (
+            <div className="floatLeft">
+              <a
+                className='avoidDefaultLinkColor'
+                href="https://github.com/delalama"
+                rel="nofollow noopener noreferrer"
+                target="_blank"
+              >
+                <div>{name}</div>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <h1> </h1>
+      <div>
+        <Col xs={10} className="floatCenter">
+          Español (España) © 2021 Instagram from Facebook
+        </Col>
+      </div>
+    </Container>
+  );
+}
+
+export { linksName };
+
+// -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif
